b3f7b8ef3718d88057a68ad413946776658a26f9
Alexander Sklar <asklar@microsoft.com>
2021-02-27 16:03:21 -0800

    Remove unnecessary dependency on WinUI 2.x

--- package/windows/ReactNativeXaml/pch.h
+++ package/windows/ReactNativeXaml/pch.h
@@ -20,11 +20,5 @@
 
 #include <winrt/Microsoft.ReactNative.h>
 
-#include <winrt/Microsoft.UI.Xaml.Automation.Peers.h>
-#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
-#include <winrt/Microsoft.UI.Xaml.Controls.h>
-#include <winrt/Microsoft.UI.Xaml.Media.h>
-#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
 using namespace winrt::Windows::Foundation;
 #include "CppWinRTIncludes.h"
-#include <winrt/Microsoft.ReactNative.h>
