717897ff6c09a7f14069ca6b4a4f6d73223bdafc
Alexander Sklar <asklar@microsoft.com>
2021-07-04 15:48:46 -0700

    Fix GridLength to support number*, *, auto, and number

--- package/src/Props.ts
+++ package/src/Props.ts
@@ -3,7 +3,7 @@ import type * as Enums from './Enums';
 
 export type Thickness = number | { left: number, top: number, right: number, bottom: number };
 
-export type GridLength = number | '*';
+export type GridLength = number | '*' | 'auto' | `${number}*`;
 
 
 export interface NativeDependencyObjectProps extends ViewProps {
