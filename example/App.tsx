e78f0632b390e592d85950a057fef685ee51e3b7
Alexander Sklar <asklar@microsoft.com>
2021-07-22 23:15:07 -0700

    builds

--- example/App.tsx
+++ example/App.tsx
@@ -28,22 +28,30 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {HyperlinkButton, Border, TextBlock, StackPanel, Button, ComboBox, ComboBoxItem,
-NativeXamlControl,
-MenuFlyoutItem,
-TextBox,
-// BlankUserControl,
-ClickMode,
-MenuFlyout,
-RichTextBlock,
-Run,
-Italic,
-LineBreak,
-    Hyperlink,
-    NavigationView,
-    NavigationViewItem,
-NavigationViewItemHeader,
-FontIcon,
+import {
+  HyperlinkButton,
+  Border,
+  TextBlock,
+  StackPanel,
+  Button,
+  ComboBox,
+  ComboBoxItem,
+  NativeXamlControl,
+  MenuFlyoutItem,
+  TextBox,
+  // BlankUserControl,
+  ClickMode,
+  MenuFlyout,
+  RichTextBlock,
+  Run,
+  Italic,
+  LineBreak,
+  Hyperlink,
+  NavigationView,
+  NavigationViewItem,
+  NavigationViewItemHeader,
+  FontIcon,
+  WinUI_InfoBar,
 } from 'react-native-xaml';
 
 const Section = ({children, title}): Node => {
@@ -81,7 +89,7 @@ const App: () => Node = () => {
 
   const [count, setCount] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
-  const [option, setOption] = useState("");
+  const [option, setOption] = useState('');
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -89,21 +97,27 @@ const App: () => Node = () => {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <View>
-          <Text style={{fontSize: 36, textAlign: "center", padding: 42}}>
+          <Text style={{fontSize: 36, textAlign: 'center', padding: 42}}>
             Welcome to React Native XAML
           </Text>
         </View>
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                  }}>
-                    <TextBox text={option} focusable={true} />
-                  <Button foreground="#992222" onTapped={(a) => {
-                      setOption(JSON.stringify(a.nativeEvent));
-                  }}
-                  content={{ string: 'button' }} />
-              {/* <BlankUserControl  onHappened={(arg) => alert(JSON.stringify(arg.nativeEvent)) } /> */}
-                  {/* <Button content={{ string: `Last selected option = ${option} ${count}` }}
+          }}>
+          <TextBox text={option} focusable={true} />
+          <WinUI_InfoBar title="title">
+            <TextBlock text="asd"/>
+          </WinUI_InfoBar>
+          <Button
+            foreground="#992222"
+            onTapped={a => {
+              setOption(JSON.stringify(a.nativeEvent));
+            }}
+            content={{string: 'button'}}
+          />
+          {/* <BlankUserControl  onHappened={(arg) => alert(JSON.stringify(arg.nativeEvent)) } /> */}
+          {/* <Button content={{ string: `Last selected option = ${option} ${count}` }}
                       onClick={(a) => { 
                         alert(JSON.stringify(a.nativeEvent)); 
                         setCount(count + 1); 
@@ -114,39 +128,47 @@ const App: () => Node = () => {
                         ButtonForegroundPressed: "#2090ff",
                      }}
                   /> */}
-                  <TextBlock foreground="black" padding={20} margin={20}>
-                    <Run text="hello world!"/>
-                    <LineBreak />
-                    <Italic>
-                     <Run text="hi there"/>
-                    </Italic>
-                    <Hyperlink navigateUri="http://bing.com">
-                      <Run text="Go to bing"/>
-                    </Hyperlink>
-                  </TextBlock>
-                  <NavigationView style={{ height: 200, width: 120, margin: 20, padding: 40 }}>
-                      <NavigationViewItem content={{ string: "item 1" }} >
-                        <FontIcon glyph="&#xE790;" />
-                      </NavigationViewItem>
-                      <NavigationViewItem content={{ string: "item 2" }} />
-                  </NavigationView>
-                  {/*<StackPanel orientation="horizontal">*/}
-                  {/*    <HyperlinkButton content={{ string: "Click me!" }} onClick={(args) => {*/}
-                  {/*        alert(`clicked! Native event args: ${JSON.stringify(args.nativeEvent)}`);*/}
-                  {/*    }} />*/}
-                  {/*    <Border verticalAlignment="center" background="paleturquoise" >*/}
-                  {/*        <TextBlock text="this is a textblock" foreground='red' textAlignment="center" />*/}
-                  {/*    </Border>*/}
-                  {/*    <TextBlock text="this is another textblock" foreground='green' textAlignment="center" />*/}
-                  {/*    <Button content={{ string: "this is a button" }} onClick={() => { alert("you clicked the button!"); }} />*/}
-                  {/*</StackPanel>*/}
-                  <ComboBox text="this is a combobox" description={{ string: "best bois" }} onSelectionChanged={(args) => {
-                      alert(`sel changed! Native event args: ${JSON.stringify(args.nativeEvent)}`); }
-                  } >
-                      <ComboBoxItem content={{ string: "garfield" }} foreground="black" />
-                      <ComboBoxItem content={{ string: "snoopy" }} foreground="black" />
-                 </ComboBox>
-                  {/* <TextBox text="this is a textbox with a menuFlyout" foreground="red">
+          <TextBlock foreground="black" padding={20} margin={20}>
+            <Run text="hello world!" />
+            <LineBreak />
+            <Italic>
+              <Run text="hi there" />
+            </Italic>
+            <Hyperlink navigateUri="http://bing.com">
+              <Run text="Go to bing" />
+            </Hyperlink>
+          </TextBlock>
+          <NavigationView
+            style={{height: 200, width: 120, margin: 20, padding: 40}}>
+            <NavigationViewItem content={{string: 'item 1'}}>
+              <FontIcon glyph="&#xE790;" />
+            </NavigationViewItem>
+            <NavigationViewItem content={{string: 'item 2'}} />
+          </NavigationView>
+          {/*<StackPanel orientation="horizontal">*/}
+          {/*    <HyperlinkButton content={{ string: "Click me!" }} onClick={(args) => {*/}
+          {/*        alert(`clicked! Native event args: ${JSON.stringify(args.nativeEvent)}`);*/}
+          {/*    }} />*/}
+          {/*    <Border verticalAlignment="center" background="paleturquoise" >*/}
+          {/*        <TextBlock text="this is a textblock" foreground='red' textAlignment="center" />*/}
+          {/*    </Border>*/}
+          {/*    <TextBlock text="this is another textblock" foreground='green' textAlignment="center" />*/}
+          {/*    <Button content={{ string: "this is a button" }} onClick={() => { alert("you clicked the button!"); }} />*/}
+          {/*</StackPanel>*/}
+          <ComboBox
+            text="this is a combobox"
+            description={{string: 'best bois'}}
+            onSelectionChanged={args => {
+              alert(
+                `sel changed! Native event args: ${JSON.stringify(
+                  args.nativeEvent,
+                )}`,
+              );
+            }}>
+            <ComboBoxItem content={{string: 'garfield'}} foreground="black" />
+            <ComboBoxItem content={{string: 'snoopy'}} foreground="black" />
+          </ComboBox>
+          {/* <TextBox text="this is a textbox with a menuFlyout" foreground="red">
                       <MenuFlyout isOpen={isOpen} onClosed={() => {
                           setIsOpen(false);
                       }} >
@@ -155,12 +177,12 @@ const App: () => Node = () => {
                     </MenuFlyout>
                   </TextBox> */}
 
-                  {/*<TextBox text="this is a textbox with a flyout" */}
-                  {/*  foreground="red" >*/}
-                  {/*  <NativeXamlControl type="flyout" >*/}
-                  {/*    <Button content={{string: "click me"}} />*/}
-                  {/*  </NativeXamlControl>*/}
-                  {/*</TextBox>*/}
+          {/*<TextBox text="this is a textbox with a flyout" */}
+          {/*  foreground="red" >*/}
+          {/*  <NativeXamlControl type="flyout" >*/}
+          {/*    <Button content={{string: "click me"}} />*/}
+          {/*  </NativeXamlControl>*/}
+          {/*</TextBox>*/}
 
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
