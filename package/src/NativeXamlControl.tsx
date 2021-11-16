c19c9769b0c7c44e52d6e8dea9790a899eb5b139
Alexander Sklar <asklar@microsoft.com>
2021-11-15 17:18:22 -0800

    >

--- package/src/NativeXamlControl.tsx
+++ package/src/NativeXamlControl.tsx
@@ -1,10 +1,6 @@
 // @refresh reset
 
-import React from "react";
 import { requireNativeComponent } from "react-native";
 import type { XamlControlProps } from './Props';
 
 export const NativeXamlControl = requireNativeComponent<XamlControlProps>("XamlControl");
-
-export class NativeXamlControlCC extends React.Component<XamlControlProps> {
-}
