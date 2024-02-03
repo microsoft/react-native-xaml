c9cef23729719ac207be9497dd23ab4ae6d2293d
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2024-02-02 16:08:04 -0800

    Upgrade to RNW 0.73

--- examplenuget/metro.config.js
+++ examplenuget/metro.config.js
@@ -1,9 +1,5 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
 const fs = require('fs');
 const path = require('path');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
@@ -12,13 +8,14 @@ const rnwPath = fs.realpathSync(
   path.resolve(require.resolve('react-native-windows/package.json'), '..'),
 );
 
-module.exports = {
-  watchFolders: [
-    // Include hoisted modules
-    path.resolve(__dirname, '..', 'node_modules'),
-    // Include main react-native-xaml package root
-    path.resolve(__dirname, '..', 'package'),
-  ],
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const config = {
   resolver: {
     blockList: exclusionList([
       // This stops "react-native run-windows" from causing the metro server to crash if its already running
@@ -47,3 +44,5 @@ module.exports = {
     assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
