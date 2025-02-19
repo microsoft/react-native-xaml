a81f1988d715b3425456805cc3984d187d89f630
Jon Thysell <jthysell@microsoft.com>
2025-02-18 17:00:11 -0800

    try to fix examplenuget yarn setup

--- examplenuget/metro.config.js
+++ examplenuget/metro.config.js
@@ -8,6 +8,8 @@ const rnwPath = fs.realpathSync(
   path.resolve(require.resolve('react-native-windows/package.json'), '..'),
 );
 
+//
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -16,22 +18,19 @@ const rnwPath = fs.realpathSync(
  */
 
 const config = {
+  //
   resolver: {
     blockList: exclusionList([
-      // This stops "react-native run-windows" from causing the metro server to crash if its already running
+      // This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
       new RegExp(
         `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
       ),
-      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+      // This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
       new RegExp(`${rnwPath}/build/.*`),
       new RegExp(`${rnwPath}/target/.*`),
       /.*\.ProjectImports\.zip/,
     ]),
-    extraNodeModules: {
-      'react-native-xaml': fs.realpathSync(
-        path.dirname(require.resolve('react-native-xaml/package.json')),
-      ),
-    },
+    //
   },
   transformer: {
     getTransformOptions: async () => ({
@@ -40,8 +39,6 @@ const config = {
         inlineRequires: true,
       },
     }),
-    // This fixes the 'missing-asset-registry-path` error (see https://github.com/microsoft/react-native-windows/issues/11437)
-    assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
   },
 };
 
