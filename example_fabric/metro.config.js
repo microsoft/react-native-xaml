ea7484bb0f17c4fd293996272c4148e360cdedd3
JesseCol <50150435+JesseCol@users.noreply.github.com>
2025-02-24 09:39:43 -0800

    Add example_fabric app

---
+++ example_fabric/metro.config.js
@@ -0,0 +1,45 @@
+const MetroConfig = require('@rnx-kit/metro-config');
+
+const fs = require('fs');
+const path = require('path');
+
+const rnwPath = fs.realpathSync(
+  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
+);
+
+//
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const config = {
+  //
+  resolver: {
+    blockList: MetroConfig.exclusionList([
+      // This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+      new RegExp(`${rnwPath}/build/.*`),
+      new RegExp(`${rnwPath}/target/.*`),
+      /.*\.ProjectImports\.zip/,
+    ]),
+    //
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  projectRoot: __dirname,
+};
+
+module.exports = MetroConfig.makeMetroConfig(config);
